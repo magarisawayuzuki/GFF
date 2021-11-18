391252200c277c817bb901e7c33fa7ff5b34e8e1
yuzuki <ymagarisawa@gmail.com>
2021-11-18 15:01:51 +0900

    ダメージ処理途中　Enemyの倍率処理中

--- Assets/Scripts/Controller/CharaSc/EnemyController.cs
+++ Assets/Scripts/Controller/CharaSc/EnemyController.cs
@@ -7,6 +7,14 @@ using UnityEngine;
 /// </summary>
 public class EnemyController : CharacterController
 {
+    [Header("普通の敵のダメージ倍率")]
+    [SerializeField] private float[] nomalScale = { 0, 0 };
+
+    [Header("柔らかい敵のダメージ倍率")]
+    [SerializeField] private float[] plantScale = { 0, 0 };
+
+    [Header("硬い敵のダメージ倍率")]
+    [SerializeField] private float[] rockScale = {0, 0};
 
     /// <summary>
     /// 子クラスのinputを用いてinputを返す。ここでは共通処理のみ記述可
@@ -16,6 +24,44 @@ public class EnemyController : CharacterController
     {
 
         return input;
+    }
+
+    public override void CharaLifeCalculation(int damage, int knockBack, int weapon)
+    {
+        int damageScaleSword = 1;
+        int damageScaleHammer = 1;
+        //子クラスにEnemyRockがあれば実行
+        if (this.GetType().IsSubclassOf(typeof(EnemyRock)))
+        {
+            damageScaleSword = 0;
+            damageScaleHammer = 0;
+        }
+        //子クラスにEnemyPlantがあれば実行
+        else if (this.GetType().IsSubclassOf(typeof(EnemyPlant)))
+        {
+
+        }
+        //子クラスにEnemyNormalがあれば実行
+        else if(this.GetType().IsSubclassOf(typeof(EnemyNormal)))
+        {
+
+        }
+
+        switch (weapon)
+        {
+            //剣
+            case 0:
+                damage *= 0;
+                break;
+
+            //槌
+            case 1:
+                damage *= 0;
+                break;
+        }
+
+
+        base.CharaLifeCalculation(damage, knockBack,weapon);
     }
 
     //記憶の欠片を落とす処理を追記
