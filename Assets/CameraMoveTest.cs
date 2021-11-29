b02b73784dba8a5712b5608107ca9d491b3b37a4
yuzuki <ymagarisawa@gmail.com>
2021-11-29 15:03:23 +0900

    Camera挙動途中

---
+++ Assets/CameraMoveTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMoveTest : MonoBehaviour
+{
+    private GameObject playerChara = default;
+    private bool _isOutZone = false;
+    private Vector3 nowCameraPos = default;
+    private Vector3 RESET_Z_POS = new Vector3(1, 1, 0);
+    private float twoDistance = default;
+
+    private void Start()
+    {
+        playerChara = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        nowCameraPos = this.gameObject.transform.position;
+        //二点の差
+        print(Vector3.Distance(this.gameObject.transform.position, playerChara.transform.position));
+    }
+
+}
