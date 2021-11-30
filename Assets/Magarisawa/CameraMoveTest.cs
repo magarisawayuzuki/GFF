84c0d3b67a9d7bfa216921d3480aa648b5a86e69
yuzuki <ymagarisawa@gmail.com>
2021-11-30 14:55:06 +0900

    camera~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- Assets/Magarisawa/CameraMoveTest.cs
+++ Assets/Magarisawa/CameraMoveTest.cs
@@ -4,37 +4,27 @@ using UnityEngine;
 
 public class CameraMoveTest : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject target;
-
     private GameObject playerChara = default;
+
     private Vector3 nowCameraPos = default;
     private Vector3 RESET_Z_POS = new Vector3(1, 1, 0);
-    private const int DISTANCE_STANDARD_VALUE = 3;
-    private const int DISTANCE_STOP_AREA_VALUE = 3;
+    
+    private const float DISTANCE_STANDARD_VALUE = 0.7f;
+    private const float DISTANCE_STOP_AREA_VALUE = 0.5f;
+    private const float CAMERA_MOVE_SPEED = 0.04f;
 
-    private bool _isTracking = false;
-    Vector3 a;
+    private bool _inVerticalArea = false;
 
     private void Start()
     {
         playerChara = GameObject.FindGameObjectWithTag("Player");
 
         RESET_Z_POS = new Vector3(0, 0, this.gameObject.transform.position.z - playerChara.gameObject.transform.position.z);
-        a = this.transform.position;
-
     }
 
     private void Update()
     {
-        print(target.transform.position);
-        print(this.gameObject.transform.position);
-
-
-        this.gameObject.transform.position = Vector3.Lerp(a, target.transform.position, 0.04f);
-
-        //CheckDistance();
-        //MoveCamera();
+        CheckDistance();
     }
 
     /// <summary>
@@ -45,38 +35,34 @@ public class CameraMoveTest : MonoBehaviour
         //カメラの位置
         Vector3 cameraNowPos = this.gameObject.transform.position;
         //z座標を補完したカメラの位置
-        Vector3 adjustmentNowPos = this.gameObject.transform.position - RESET_Z_POS;
+        //Vector3 adjustmentNowPos = this.gameObject.transform.position - RESET_Z_POS;
 
         //二点の差
-        float distance = Vector3.Distance(adjustmentNowPos, playerChara.transform.position);
+        float distance = Vector3.Distance(cameraNowPos, playerChara.transform.position);
+        print(distance);
 
-        //追跡開始フラグ
-        //差が追跡開始値よりも大きい
-        if (distance > DISTANCE_STANDARD_VALUE && !_isTracking)
-        {
-            _isTracking = false;
-        }
-
-        //追跡終了フラグ
-        //差が追跡終了値よりも小さい
-        else if (distance < DISTANCE_STOP_AREA_VALUE && _isTracking)
-        {
-            _isTracking = true;
-        }
+        //Distanceを基にカメラを動かす
+        MoveCamera(distance);
     }
 
     /// <summary>
     /// カメラの挙動制御
     /// </summary>
-    private void MoveCamera()
+    private void MoveCamera(float distance)
     {
-        //開始
-        if (_isTracking)
+        //メイン挙動処理
+        float adjustmentSpeed = distance * CAMERA_MOVE_SPEED;
+        if(adjustmentSpeed <= 0.03f)
         {
-            
+            adjustmentSpeed = 0.03f;
         }
 
-        //終了
+        //上下動作無し
+        if (!_inVerticalArea)
+        {
+            this.gameObject.transform.position = Vector3.Lerp(this.transform.position, playerChara.transform.position, adjustmentSpeed);
+        }
+        //上下動作あり
         else
         {
 
