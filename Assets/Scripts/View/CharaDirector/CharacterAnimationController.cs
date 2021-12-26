64f09cc5a3a27ae897931e3e1734e61bb27ceda4
magarisawayuzuki <ymagarisawa@gmail.com>
2021-12-26 14:23:51 +0900

    Animation流す部分は大体完成

--- Assets/Scripts/View/CharaDirector/CharacterAnimationController.cs
+++ Assets/Scripts/View/CharaDirector/CharacterAnimationController.cs
@@ -16,6 +16,23 @@ public class CharacterAnimationController : MonoBehaviour
         Damage,
     }
 
+    private Animation anima;
+
+    private List<AnimationClip> animas = new List<AnimationClip>();
+
+    private void Start()
+    {
+        anima = GameObject.FindGameObjectWithTag("Player").gameObject.GetComponent<Animation>();
+
+        //animationを取得
+        foreach(AnimationState anim in anima)
+        {
+            this.animas.Add(anim.clip);
+        }
+    }
+
+
+
     /// <summary>
     /// アニメーションを切り替える判定を行う
     /// </summary>
@@ -25,35 +42,36 @@ public class CharacterAnimationController : MonoBehaviour
         switch ((int)charaStats)
         {
             case (int)copy_status.Idle:
-
+                anima.Play(this.animas[0].name);
 
                 return;
             case (int)copy_status.Move:
-
+                anima.Play(this.animas[1].name);
 
                 return;
             case (int)copy_status.Jump:
-
+                anima.Play(this.animas[2].name);
 
                 return;
             case (int)copy_status.Fall:
 
+                //anima.Play(this.animas[0].name);
 
                 return;
             case (int)copy_status.swordAttack:
-
+                anima.Play(this.animas[4].name);
 
                 return;
             case (int)copy_status.hammerAttack:
-
+                anima.Play(this.animas[5].name);
 
                 return;
             case (int)copy_status.Death:
-
+                anima.Play(this.animas[7].name);
 
                 return;
             case (int)copy_status.Damage:
-
+                anima.Play(this.animas[6].name);
 
                 return;
         }
